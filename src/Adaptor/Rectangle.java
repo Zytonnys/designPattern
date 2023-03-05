b3451d0a2b43d7a1745f5ceeb76deec67f8c304c
Zytonnys <86301601+Zytonnys@users.noreply.github.com>
2023-03-05 21:10:54 +0600

    Create Rectangle.java

---
+++ src/Adaptor/Rectangle.java
@@ -0,0 +1,9 @@
+package Adaptor;
+
+public class Rectangle {
+
+    public void draw(int x, int y, int width, int height) {
+        System.out.printf("Rectangle with coord: " + "left down point (" + x + "," + y + "), " +
+                "width: " + width + ", height: " + height);
+    }
+}
